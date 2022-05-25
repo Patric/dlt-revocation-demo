7fa10d66be6c17d248cc366a212bc409c6e6e7f1
Patryk Sulej <p.sulej@prometheus-sa.com>
2022-05-25 02:50:34 +0200

    crypto fixes

--- py-demo/app/utils/crypto.py
+++ py-demo/app/utils/crypto.py
@@ -1,4 +1,3 @@
-import json
 from Crypto.Hash import SHA256
 
 def calculateHash(data) -> str:
@@ -8,13 +7,6 @@ def calculateHash(data) -> str:
     h = SHA256.new()
     h.update(data)
     return h.hexdigest()
-
-def transaction_to_bytes(transaction_data: dict):
-    new_transaction_data = transaction_data.copy()
-    new_transaction_data["sender"] = str(transaction_data["sender"])
-    new_transaction_data["receiver"] = str(transaction_data["receiver"])
-    new_transaction_data["coin_id"] = str(transaction_data["coin_id"])
-    return json.dumps(new_transaction_data, indent=2).encode('utf-8')
 
 def encrypt(data, publicKey) -> str:
     # use asymmetric encryption
